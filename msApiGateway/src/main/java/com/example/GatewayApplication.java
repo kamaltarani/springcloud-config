93f098dd1713725ce99f34302c620c3312737c70
admin <admin@192.168.1.10>
2016-08-31 17:33:10 +0530

    API Gateway Usine ZUUL

---
+++ msApiGateway/src/main/java/com/example/GatewayApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	@Bean
+	public Filter shallowEtagHeaderFilter(){
+		return new ShallowEtagHeaderFilter();
+	}
+}
